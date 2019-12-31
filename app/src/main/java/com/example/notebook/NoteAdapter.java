b4da9a9ece63ffe63c68034ec463505fe24794f0
Osakpolor Obaseki <obasekiosa@gmail.com>
2019-12-31 15:47:24 +0100

    Optimize NoteAdapter

--- app/src/main/java/com/example/notebook/NoteAdapter.java
+++ app/src/main/java/com/example/notebook/NoteAdapter.java
@@ -12,6 +12,12 @@ import java.util.ArrayList;
 
 public class NoteAdapter extends ArrayAdapter<Note> {
 
+    public static class ViewHolder {
+        TextView title;
+        TextView noteBody;
+        ImageView noteIcon;
+    }
+
     public NoteAdapter(Context context, ArrayList<Note> notes) {
         super(context, 0, notes);
     }
@@ -20,17 +26,29 @@ public class NoteAdapter extends ArrayAdapter<Note> {
     public View getView(int position, View convertView, ViewGroup parent) {
         Note note = getItem(position);
 
+        // create a new viewHolder
+        ViewHolder viewHolder;
+
         if (convertView == null) {
+
+            // to save our view references to
+            viewHolder = new ViewHolder();
+
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_row, parent, false);
+
+            viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_title);
+            viewHolder.noteBody = (TextView) convertView.findViewById(R.id.list_item_body);
+            viewHolder.noteIcon = (ImageView) convertView.findViewById(R.id.list_item_note_img);
+
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView noteTitle = (TextView) convertView.findViewById(R.id.list_item_title);
-        TextView noteText = (TextView) convertView.findViewById(R.id.list_item_body);
-        ImageView noteIcon = (ImageView) convertView.findViewById(R.id.list_item_note_img);
-
-        noteTitle.setText(note.getTitle());
-        noteText.setText(note.getMessage());
-        noteIcon.setImageResource(note.getAssociatedDrawable());
+        viewHolder.title.setText(note.getTitle());
+        viewHolder.noteBody.setText(note.getMessage());
+        viewHolder.noteIcon.setImageResource(note.getAssociatedDrawable());
 
         return convertView;
     }

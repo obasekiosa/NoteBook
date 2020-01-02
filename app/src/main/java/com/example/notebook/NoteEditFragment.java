26d6a092a651832e933181e9276b21c52bc2d438
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-01-02 11:55:27 +0100

    Inflate and setup NoteEditFragment

--- app/src/main/java/com/example/notebook/NoteEditFragment.java
+++ app/src/main/java/com/example/notebook/NoteEditFragment.java
@@ -1,6 +1,7 @@
 package com.example.notebook;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 /**
@@ -24,8 +28,26 @@ public class NoteEditFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+
+        // Inflate fragment edit layout
+        View fragmentLayout = inflater.inflate(R.layout.fragment_note_edit, container, false);
+
+        // grab widget references from layout
+        EditText title = fragmentLayout.findViewById(R.id.edit_note_title);
+        EditText body = fragmentLayout.findViewById(R.id.edit_note_message);
+        ImageButton noteCatButton = fragmentLayout.findViewById(R.id.edit_note_button);
+        Button saveButton = fragmentLayout.findViewById(R.id.save_note_button);
+
+        // populate widget with note data
+        Intent intent = getActivity().getIntent();
+        title.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
+        body.setText(intent.getExtras().getString(MainActivity.NOTE_MESSAGE_EXTRA));
+
+        Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+        noteCatButton.setImageResource(Note.categoryToDrawable(noteCat));
+
+        // return fragment layout
+        return fragmentLayout;
     }
 
 }
